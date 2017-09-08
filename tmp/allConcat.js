30bc2d1af6fd7a12b94da61c8489296e0dab81f8
rickmutua <rick.mutua12@gmail.com>
2017-09-08 16:43:15 +0300

    worked on html, display.js and interface.js

---
+++ tmp/allConcat.js
@@ -0,0 +1,22 @@
+  var Search = require('./../js/display.js').searchModule;
+
+$('Button') .click(function () {
+  $('#hiddenBody') .show();
+});
+
+  $('Button') .click(function () {
+    $('#landingPage') .hide();
+  });
+
+
+
+
+  $(document).ready(function() {
+    $('#search').submit(function(event){
+      event.preventDefault();
+      var userName = $('#search').val();
+      console.log(userName);
+      userSearch = new Search(userName);
+      userSearch.getRepos(userName);
+    });
+  });
